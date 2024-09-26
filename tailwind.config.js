15368ed7b1f1df8d60657478433e48990ab888e4
Eng Yusuf Ali Amalow <yusufaliamalow@gmail.com>
2024-09-26 17:33:55 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      
+      fontFamily: {
+        poppins: ['Roboto', 'sans-serif'],
+        Gowun :["Gowun Batang"  , "serif"]
+      },
+    },
+  },
+  plugins: [],
+}
+
